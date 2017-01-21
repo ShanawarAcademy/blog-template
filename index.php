f5e09698e97a01b6671f1969199eabeb8668116e
ShanawarAcademy <shanawar78@gmail.com>
2017-01-21 22:00:45 +0500

    Blog Template
    This is the updated template.

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+
+<header>
+
+<section id="logo">
+
+<img src="images/cms11.png" width="400" height="120" style="margin-top:0px;"/>
+
+</section>
+
+<nav>
+
+  <a href="#"><li style="margin-left:10px; background:orange;">Home</li></a>
+  <a href="#"> <li>Food</li> </a>
+  <a href="#"> <li>Technology</li> </a>
+  <a href="#"> <li>Health</li> </a>
+  <a href="#"> <li>Development</li> </a>
+  <a href="#"> <li>About Us</li> </a>
+
+</nav>
+
+</header>
+
+<section id="wrapper">
+
+
+
+<section id="content">
+ <section id="artical">
+ 
+<span style="color:orange; font-family:Tahoma, Geneva, sans-serif; font-weight:bold; font-size:20px;">Easy way to break Android phone's pattern lock </span>
+
+<p>
+<img src="images/android pattern.png" width="310" height="200" style="float:left; margin-right:10px;" />
+<section id="description">
+People who use Android smart phone in order to save valuable data they often use the pattern lock and it is a very useful feature. But forgetting the pattern lock cause anxiety for many people and then you need google account to unlock the mobile phone. But if you forgot Google account username and password.it becomes impossible to open the pattern lock. Don't worry because we will tell you how you can break the pattern lock without Google Account. But remember that using this method all data in your mobile phone memory will be deleted. However, if you are using the memory card it will be completely safe.
+</section>
+
+<input type="submit" id="read-more" value="Read More" />
+</p>
+</section>
+
+<section id="artical">
+ 
+<span style="color:orange; font-family:Tahoma, Geneva, sans-serif; font-weight:bold; font-size:20px;">Easy way to break Android phone's pattern lock </span>
+
+<p>
+<img src="images/android.jpg" width="310" height="200" style="float:left; margin-right:10px;" />
+
+<section id="description">
+People who use Android smart phone in order to save valuable data they often use the pattern lock and it is a very useful feature. But forgetting the pattern lock cause anxiety for many people and then you need google account to unlock the mobile phone. But if you forgot Google account username and password.it becomes impossible to open the pattern lock. Don't worry because we will tell you how you can break the pattern lock without Google Account. But remember that using this method all data in your mobile phone memory will be deleted. However, if you are using the memory card it will be completely safe.
+</section>
+<input type="submit" id="read-more" value="Read More" />
+</p>
+</section>
+
+<section id="artical">
+ 
+<span style="color:orange; font-family:Tahoma, Geneva, sans-serif; font-weight:bold; font-size:20px;">Easy way to break Android phone's pattern lock </span>
+
+<p>
+<img src="images/android pattern.png" width="310" height="200" style="float:left; margin-right:10px;" />
+<section id="description">
+People who use Android smart phone in order to save valuable data they often use the pattern lock and it is a very useful feature. But forgetting the pattern lock cause anxiety for many people and then you need google account to unlock the mobile phone. But if you forgot Google account username and password.it becomes impossible to open the pattern lock. Don't worry because we will tell you how you can break the pattern lock without Google Account. But remember that using this method all data in your mobile phone memory will be deleted. However, if you are using the memory card it will be completely safe.
+</section>
+<input type="submit" id="read-more" value="Read More" />
+</p>
+</section>
+
+<section id="artical">
+ 
+<span style="color:orange; font-family:Tahoma, Geneva, sans-serif; font-weight:bold; font-size:20px;">Easy way to break Android phone's pattern lock </span>
+
+<p>
+<img src="images/android.jpg" width="310" height="200" style="float:left; margin-right:10px;" />
+<section id="description">
+People who use Android smart phone in order to save valuable data they often use the pattern lock and it is a very useful feature. But forgetting the pattern lock cause anxiety for many people and then you need google account to unlock the mobile phone. But if you forgot Google account username and password.it becomes impossible to open the pattern lock. Don't worry because we will tell you how you can break the pattern lock without Google Account. But remember that using this method all data in your mobile phone memory will be deleted. However, if you are using the memory card it will be completely safe.
+</section>
+<input type="submit" id="read-more" value="Read More" />
+</p>
+</section>
+
+
+</section>
+
+<aside>
+
+<section id="search-area">
+Search
+
+<section id="search-box">
+<input type="search" id="searchbar" name="search" placeholder="Search ..."/>
+<input type="submit" id="searchbtn" value="Search" />
+</section>
+</section>
+
+<br>
+<span style="color:orange; font-family:Tahoma, Geneva, sans-serif; margin-left:5px; font-weight:bold; font-size:18px;">Recent Posts</span>
+
+<section id="recent-posts">
+
+<section id="re-posts">
+<img src="images/android.jpg" width="50" height="50" style="float:left; margin-right:10px;" />
+Easy way to break Android phone's pattern lock
+</section>
+
+<section id="re-posts">
+<img src="images/android pattern.png" width="50" height="50" style="float:left; margin-right:10px;" />
+Easy way to break Android phone's pattern lock
+</section>
+
+<section id="re-posts">
+<img src="images/android.jpg" width="50" height="50" style="float:left; margin-right:10px;" />
+Easy way to break Android phone's pattern lock
+</section>
+
+<section id="re-posts">
+<img src="images/android pattern.png" width="50" height="50" style="float:left; margin-right:10px;" />
+Easy way to break Android phone's pattern lock
+</section>
+
+
+</section>
+
+
+
+</aside>
+
+
+</section>
+
+<footer>
+&copy; All rights reserved<br>
+Designed & Developed by Mr. SHANAWAR SARFRAZ
+</footer>
+
+</body>
+
+</html>
